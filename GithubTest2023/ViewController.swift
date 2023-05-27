892745c574f86969d60ecb11c4d0194ca1091c1a
RilsK <38708972+RilsK@users.noreply.github.com>
2023-05-27 19:48:29 +0900

    Initial Commit

---
+++ GithubTest2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubTest2023
+//
+//  Created by Uchida Ryusuke on 2023/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
